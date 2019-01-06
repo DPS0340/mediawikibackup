bfe168a323c97c2ff056fec2022b70cc312cb5bd
DPS0340 <jh001007@gmail.com>
2019-01-06 15:59:36 +0900

    dd

--- mediawikibackup.py
+++ mediawikibackup.py
@@ -4,6 +4,7 @@ import sys
 if __name__ == "__main__":
     sys.path.insert(0, './python-mysql-backup')
     import dbbackup
+    dbbackup.main()
     with open("secret", "r") as r:
         secret = r.readline()
     dbx = dropbox.Dropbox(secret)
