460302b2d7eeca289656f14bb225c656e41b7c3d
DPS0340 <jh001007@gmail.com>
2019-01-06 16:02:43 +0900

    dd

--- settings.py
+++ settings.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
     DB_USER = input("please type DB_USER.")
     DB_USER_PASSWORD = input("please type DB_USER_PASSWORD.")
     DB_NAME = input("please type DB_NAME.")
+    WIKI_PATH = input("please type WIKI_PATH.")
     dump = {
         "DB_HOST":DB_HOST,
         "DB_USER":DB_USER,
         "DB_USER_PASSWORD":DB_USER_PASSWORD,
-        "DB_NAME":DB_NAME
+        "DB_NAME":DB_NAME,
+        "WIKI_PATH":WIKI_PATH
     }
     with open("./settings", "wb") as w:
         pickle.dump(dump, w)
