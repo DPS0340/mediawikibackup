86bd2ecaaecb81b7318f647724a0fbd379ade16e
DPS0340 <jh001007@gmail.com>
2019-01-06 15:54:56 +0900

    dd

--- mediawikibackup.py
+++ mediawikibackup.py
@@ -1,6 +1,7 @@
 import dropbox
-
-with open("secret.txt", "r") as r:
-    secret = r.readline()
-dbx = dropbox.Dropbox(secret)
-print(dbx.users_get_current_account())
+if __name__ == "__main__":
+    from python-mysql-backup import dbbackup
+    with open("secret", "r") as r:
+        secret = r.readline()
+    dbx = dropbox.Dropbox(secret)
+    print(dbx.users_get_current_account())
