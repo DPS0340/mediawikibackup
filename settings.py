6f24bc74292fb0dd0139ccf7e484594646ebfa2a
jiho lee <jh001007@gmail.com>
2019-01-19 12:50:55 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,13 +3,14 @@ import pickle
 
 
 if __name__ == "__main__":
-    SECRET = input("please type dropbox token.")
+    SECRET = input("please type bucket id.")
     NGINX = input("please type nginx site setting file in site-available.")
     DB_HOST = input("please type DB_HOST.")
     DB_USER = input("please type DB_USER.")
     DB_USER_PASSWORD = input("please type DB_USER_PASSWORD.")
     DB_NAME = input("please type DB_NAME.")
     WIKI_PATH = input("please type wiki folder path.")
+    LOG = input("please type log file path.")
     dump = {
         "NGINX":NGINX,
         "SECRET":SECRET,
@@ -17,7 +18,8 @@ if __name__ == "__main__":
         "DB_USER":DB_USER,
         "DB_USER_PASSWORD":DB_USER_PASSWORD,
         "DB_NAME":DB_NAME,
-        "WIKI_PATH":WIKI_PATH
+        "WIKI_PATH":WIKI_PATH,
+        "LOG":LOG
     }
     with open(os.path.dirname(os.path.realpath(__file__)) + "/settings", "wb") as w:
         pickle.dump(dump, w)
