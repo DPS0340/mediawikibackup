c760e79aeb6121ee11ed568988946f6a824e0457
jiho lee <jh001007@gmail.com>
2019-01-19 13:06:11 +0900

    Update mediawikibackup.py

--- mediawikibackup.py
+++ mediawikibackup.py
@@ -6,13 +6,13 @@ import pickle
 from distutils.dir_util import copy_tree
 import tarfile
 from shutil import copyfile
+from mysqlbackup import dbbackup
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 if __name__ == "__main__":
-    from mysqlbackup import dbbackup
     backuppath, datetime = dbbackup.main()
     print("backup to dropbox...")
     with open(os.path.dirname(os.path.realpath(__file__)) + "/settings", "rb") as r:
@@ -20,8 +20,10 @@ if __name__ == "__main__":
         secret = dict["SECRET"]
         WIKI_PATH = dict["WIKI_PATH"]
         nginx = dict["NGINX"]
+        log = dict["LOG"]
     copy_tree(WIKI_PATH, backuppath + "/wiki")
     copyfile(nginx, backuppath + "/nginx")
+    copyfile(log, backuppath + "/log")
     dbx = dropbox.Dropbox(secret)
     make_tarfile(os.path.dirname(os.path.realpath(__file__)) + "/backup/" + datetime+ ".tar", backuppath)
     with open(os.path.dirname(os.path.realpath(__file__)) + "/backup/" + datetime+ ".tar", "rb") as f:
