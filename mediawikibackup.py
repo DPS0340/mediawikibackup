f4829dc9d45534b14fe140ae4af32a0f222a3dff
jiho lee <jh001007@gmail.com>
2019-01-19 12:57:36 +0900

    Update mediawikibackup.py

--- mediawikibackup.py
+++ mediawikibackup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-import dropbox
+import google.cloud.storage
 import sys
 import os
 import pickle
 from distutils.dir_util import copy_tree
 import tarfile
 from shutil import copyfile
+from mysqlbackup import dbbackup
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 if __name__ == "__main__":
-    from mysqlbackup import dbbackup
+    storage_client = google.cloud.storage.Client()
     backuppath, datetime = dbbackup.main()
     print("backup to dropbox...")
     with open(os.path.dirname(os.path.realpath(__file__)) + "/settings", "rb") as r:
@@ -22,8 +23,8 @@ if __name__ == "__main__":
         nginx = dict["NGINX"]
     copy_tree(WIKI_PATH, backuppath + "/wiki")
     copyfile(nginx, backuppath + "/nginx")
-    dbx = dropbox.Dropbox(secret)
+    bucket = storage_client.get_bucket(secret)
+    blob = bucket.blob(os.path.basename(os.path.dirname(os.path.realpath(__file__)) + "/backup/" + datetime+ ".tar"))
     make_tarfile(os.path.dirname(os.path.realpath(__file__)) + "/backup/" + datetime+ ".tar", backuppath)
-    with open(os.path.dirname(os.path.realpath(__file__)) + "/backup/" + datetime+ ".tar", "rb") as f:
-        dbx.files_upload(f.read(), "/" + datetime + ".tar", mute=True)
+    blob.upload_from_filename(os.path.dirname(os.path.realpath(__file__)) + "/backup/" + datetime+ ".tar")
     print("backup to dropbox complete!")
