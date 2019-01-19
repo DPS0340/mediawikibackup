d474feb925f5364342cc6b5938f67bb2ea98b234
jiho lee <jh001007@gmail.com>
2019-01-19 12:50:21 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     DB_USER_PASSWORD = input("please type DB_USER_PASSWORD.")
     DB_NAME = input("please type DB_NAME.")
     WIKI_PATH = input("please type wiki folder path.")
+    LOG = input("please type log file path.")
     dump = {
         "NGINX":NGINX,
         "SECRET":SECRET,
@@ -17,7 +18,8 @@ if __name__ == "__main__":
         "DB_USER":DB_USER,
         "DB_USER_PASSWORD":DB_USER_PASSWORD,
         "DB_NAME":DB_NAME,
-        "WIKI_PATH":WIKI_PATH
+        "WIKI_PATH":WIKI_PATH,
+        "LOG":LOG
     }
     with open(os.path.dirname(os.path.realpath(__file__)) + "/settings", "wb") as w:
         pickle.dump(dump, w)
